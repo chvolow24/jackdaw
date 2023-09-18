1cab8ba18f674c4ac45f40b65235257545032fcc
Charlie Volow <charlie.volow@gmail.com>
2023-09-18 13:01:49 -0400

    license

--- gui.c
+++ gui.c
@@ -1,34 +1,35 @@
+/*****************************************************************************************************************
+    Jackdaw | a stripped-down, keyboard-focused Digital Audio Workstation | built on SDL (https://libsdl.org/)
+******************************************************************************************************************
+
+    Copyright (C) 2023 Charlie Volow
+
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+        claim that you wrote the original software. If you use this software
+        in a product, an acknowledgment in the product documentation would be
+        appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be
+        misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+
+*****************************************************************************************************************/
+
 /**************************************************************************************************
- * Jackdaw | a stripped-down, keyboard-focused Digital Audio Workstation. Built on SDL.
-***************************************************************************************************
+    gui.c
 
-  Copyright (C) 2023 Charlie Volow
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-
-**************************************************************************************************/
-
-/**************************************************************************************************
- * Drawing functions
-    -> incl. "draw_project," which is called on every iteration of the animation loop.
- * Creation of GUI primitives, like Textbox. Project.c initializes some of these primitives.
- * Modification (resizing, repositioning) of GUI elements
+    * Creation of GUI primitives, like Textbox. Project.c initializes some of these primitives.
+    * Modification (resizing, repositioning) of GUI elements
+    * Drawing functions
+        -> incl. "draw_project," which is called on every iteration of the animation loop.
  **************************************************************************************************/
-
 
 #include <float.h>
 #include <math.h>

09774d892b34ba98d0886ebbe6db9fdc05f534ed
Charlie Volow <charlie.volow@gmail.com>
2023-11-05 09:55:08 -0500

    remove PATH check

--- update.sh
+++ update.sh
@@ -13,19 +13,16 @@ fi
 
 sudo mv jackdaw /usr/local/bin
 
-if [[ ":$PATH:" == *"usr/local/bin"* ]]; then
-    echo "Path already includes install dir (/usr/local/bin)."
-else
-    if ! grep -q 'export PATH="/usr/local/bin:$PATH"' ~/.bashrc; then
-        echo -e "Adding install dir to PATH in ~/.bashrc..."
-        echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-    fi
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        if ! grep -q 'export PATH="/usr/local/bin:$PATH"' ~/.bashrc; then
-            echo -e "Adding install dir to PATH in ~/.zshrc..."
-            echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
-        fi
-    fi
-    export PATH="/usr/local/bin:$PATH"
+if ! grep -q 'export PATH="/usr/local/bin:$PATH"' ~/.bashrc; then
+    echo -e "Adding install dir to PATH in ~/.bashrc..."
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 fi
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if ! grep -q 'export PATH="/usr/local/bin:$PATH"' ~/.zshrc; then
+        echo -e "Adding install dir to PATH in ~/.zshrc..."
+        echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+    fi
+fi
+export PATH="/usr/local/bin:$PATH"
+
 echo -e "\n\nDone! Run the program by typing 'jackdaw' on the command line in a bash or zsh terminal."

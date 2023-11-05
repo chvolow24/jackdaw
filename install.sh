9fc87c6bc8ae77016b0bdd7d11bd1b02fb01076b
Charlie Volow <charlie.volow@gmail.com>
2023-11-05 09:41:16 -0500

    update install scripts

--- install.sh
+++ install.sh
@@ -52,7 +52,16 @@ fi
 make
 echo -e "\n\nInstalling executable at /usr/local/bin/jackdaw..."
 if [[ ! -d "/usr/local/bin" ]]; then
-    mkdir -p /usr/local/bin
+    sudo mkdir -p /usr/local/bin
 fi
-sudo mv jackdaw /usr/local/bin/jackdaw
-echo -e "\n\nDone! Run the program by typing 'jackdaw' on the command line."
+sudo mv jackdaw /usr/local/bin
+
+echo -e "Adding install dir to PATH in ~/.bashrc..."
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo -e "Adding install dir to PATH in ~/.zshrc..."
+
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+fi
+export PATH="/usr/local/bin:$PATH"
+echo -e "\n\nDone! Run the program by typing 'jackdaw' on the command line in a bash or zsh terminal."
--- update.sh
+++ update.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 echo "Getting latest files and updating application..."
 git pull
+echo -e "Building project..."
 if [[ ! -d "build" ]]; then
     mkdir build
 fi
+make
 echo -e "\n\nInstalling executable at /usr/local/bin/jackdaw..."
 if [[ ! -d "/usr/local/bin" ]]; then
-    mkdir -p /usr/local/bin
+    sudo mkdir -p /usr/local/bin
 fi
-sudo mv jackdaw /usr/local/bin/jackdaw
-echo -e "\n\nDone! Run the program by typing 'jackdaw' on the command line."
+
+sudo mv jackdaw /usr/local/bin
+
+echo -e "Adding install dir to PATH in ~/.bashrc..."
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo -e "Adding install dir to PATH in ~/.zshrc..."
+
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+fi
+export PATH="/usr/local/bin:$PATH"
+echo -e "\n\nDone! Run the program by typing 'jackdaw' on the command line in a bash or zsh terminal."
